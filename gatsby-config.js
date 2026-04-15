39e69b954c50572ef6d5ced550aca7465023e92b
Elkin Urango <kin3xc@MacBook-Air-de-Elkin.local>
2026-04-14 20:07:48 -0500

    fix: remove fallback tracking ID for Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -53,7 +53,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: process.env.GOOGLE_ANALYTICS_ID || 'UA-75654335-1',
+        trackingId: process.env.GOOGLE_ANALYTICS_ID,
       },
     },
     `gatsby-plugin-sitemap`,
